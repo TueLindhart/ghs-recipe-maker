e25e47a9c22abbc50fc34a60a1164aeb7d03cca9
Tue Thomsen <tuethomsen28@gmail.com>
2023-05-30 21:41:15 +0200

    SQL co2 prompttemplate

--- chains/co2_sql.py
+++ chains/co2_sql.py
@@ -2,30 +2,25 @@ from typing import Literal
 
 from langchain.chains import SQLDatabaseChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.prompt import PromptTemplate
 from langchain.sql_database import SQLDatabase
 
 from prompt_templates.co2_sql_prompts import (
-    DK_CO2_SQL_PROMPT_WITH_EXAMPLES,
-    EN_CO2_SQL_PROMPT_WITH_EXAMPLES,
+    DK_CO2_SQL_PROMPT_TEMPLATE,
+    EN_CO2_SQL_PROMPT_TEMPLATE,
 )
 
 
 def get_en_co2_sql_chain(language: Literal["da", "en"], verbose: bool = False):
     sql_dk_co2_db = SQLDatabase.from_uri("sqlite:///data/dk_co2_emission.db", sample_rows_in_table_info=2)
-    prompt = PromptTemplate(
-        input_variables=["input", "table_info", "dialect"],
-        template=EN_CO2_SQL_PROMPT_WITH_EXAMPLES if language == "en" else DK_CO2_SQL_PROMPT_WITH_EXAMPLES,
-    )
     llm = ChatOpenAI(  # type: ignore
         temperature=0,
     )
-    en_co2_sql_chain = SQLDatabaseChain.from_llm(
+    co2_sql_chain = SQLDatabaseChain.from_llm(
         llm=llm,
         db=sql_dk_co2_db,
         verbose=verbose,
-        prompt=prompt,
+        prompt=EN_CO2_SQL_PROMPT_TEMPLATE if language == "en" else DK_CO2_SQL_PROMPT_TEMPLATE,
         top_k=100,
     )
 
-    return en_co2_sql_chain
+    return co2_sql_chain
--- prompt_templates/co2_sql_prompts.py
+++ prompt_templates/co2_sql_prompts.py
@@ -1,34 +1,21 @@
-EN_CO2_SQL_PROMPT_WITH_EXAMPLES = """Given a list of ingredients,
-            extract the main ingredients from the list and create a syntactically correct {dialect} query to run,
-            then look at the results of the query and return the answer.
+from langchain import PromptTemplate
 
-Solve the task using the following steps:
-- Query all ingredients in a single query.
-  Example query: 'SELECT Name, Total_kg_CO2_eq_kg FROM dk_co2_emission
-  WHERE Name LIKE '%tomato%' OR NAME LIKE '%bouillon%'
-- In the query, remove all non-ingredient words.
-  Example of removing: '%chopped tomatoes%' to '%tomato%' or '%minced beef%' to '%beef%'
-- Match the SQLResult to the list of ingredients based on preparation and type.
-  Example match: '1 can of chopped tomatoes' best matches results from 'Tomato, peeled, canned'.
-- Return the Answer in the following format: ''ingredient': X kg CO2e / kg'.
-  Example Answer: 'Chopped tomatoes: X kg CO2e/ kg \n'.
-- If the ingredient is not found in the database, return '?'.
-  Example Answer: 'Chopped tomatoes: ? \n'
-- Do not provide any ranges for the final answer. For example, do not provide '0.1-0.5 kg CO2e per kg' as the final answer. 
-  Instead, return the closest match. 
+EN_EXAMPLE_QUERY = "'SELECT Name, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE Name LIKE '%tomato%' OR Name LIKE '%bouillon%'"
+DK_EXAMPLE_QUERY = "'SELECT Navn, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE Navn LIKE '%tomat%' OR Navn LIKE '%bouillion%'"
 
-Use the following format:
-Ingredients: "Ingredients here"
-SQLQuery: "SQL Query to run"
-SQLResult: "Result of the SQLQuery"
-Answer: "Final answer here"
+EN_EXAMPLE_REMOVING = "'SELECT Navn, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE Navn LIKE '%tomato%' OR Navn LIKE '%bouillion%'"
+DK_EXAMPLE_REMOVING = "'%hakkede tomater%' to '%tomat%' or '%hakket oksekød%' to '%oksekød%'"
 
-Only use the following tables:
-{table_info}
+EN_EXAMPLE_MATCH = "'1 can of chopped tomatoes' best matches results from 'Tomato, peeled, canned'."
+DK_EXAMPLE_MATCH = "'1 dåse hakkede tomater' best matches results from 'Tomat, flået, konserves'."
 
-Begin!
+EN_EXAMPLE_ANSWER_FOUND = "'Chopped tomatoes: X kg CO2e / kg \n'"
+DK_EXAMPLE_ANSWER_FOUND = "'Hakkede tomater: X kg CO2e/ kg \n'."
 
-Ingredients:
+EN_EXAMPLE_ANSWER_NOT_FOUND = "'Chopped tomatoes: ? \n'"
+DK_EXAMPLE_ANSWER_NOT_FOUND = "'Hakkede tomater: ? \n'."
+
+EN_INGREDIENTS_EXAMPLE = """
 150 g red lentils
 1 can of chopped tomatoes
 2 cubes of vegetable bouillon
@@ -36,66 +23,9 @@ Ingredients:
 1 tbsp. lemon juice
 1. tbsp. chili powder
 1 starfruit
+"""
 
-SQLQuery: SELECT Name, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE
-          Name LIKE '%tomato%' OR
-          Name LIKE '%lentil%' OR
-          Name LIKE '%bouillon%' OR
-          Name LIKE '%juice%' OR
-          Name LIKE '%lemon%' OR
-          Name LIKE '%chili%' OR
-          Name LIKE '%starfruit%'
-
-SQLResult: [('Tomato, ripe, raw, origin unknown', 0.7), ('Green lentils, dried', 1.78)
-            ('Tomatojuice, canned', 1.26), ('Tomato, peeled, canned', 1.26)
-            ('Tomato paste, concentrated', 2.48), ('Red lentils, dried', 1.78
-            ('Ice, popsickle, lemonade', 1.15), ('Lemon, raw', 0.94
-            ('Apple juice', 1.64),('Bouillon, chicken, prepared', 0.38)
-            ('Bouillon, beef, prepared', 0.52), ('Pepper, hot chili, raw', 1.02)
-            ('Pepper, hot chili, canned', 1.54),
-            ]
-
-Answer:
-Red lentils: 1.78 kg CO2e / kg
-Chopped tomatoes: 1.26 kg CO2e / kg
-Vegetable bouillon: 0.38 kg CO2e / kg (closest was chicken bouillon)
-Tomato concentrate (140 g): 2.48 kg CO2e / kg (closest was tomato paste)
-Lemon juice: 0.94 kg CO2e / kg (Closest was Lemon, raw)
-Starfruit: ? (Not found in database)
-
-Ingredients: {input}"""
-
-DK_CO2_SQL_PROMPT_WITH_EXAMPLES = """
-Given a list of ingredients in Danish, extract the main ingredients from the list
-and create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-
-Solve the task using the following steps:
-- Query all ingredients in a single query.
-  Example query: 'SELECT Navn, Total_kg_CO2_eq_kg FROM dk_co2_emission
-  WHERE Navn LIKE '%tomato%' OR Navn LIKE '%bouillion%'
-- In the query, remove all non-ingredient words.
-  Example of removing: '%hakkede tomater%' to '%tomat%' or '%hakket oksekød%' to '%oksekød%'
-- Match the SQLResult to the list of ingredients based on preparation and type.
-  Example match: '1 dåse hakkede tomater' best matches results from 'Tomat, flået, konserves'.
-- Return the Answer in the following format: ''ingredient': X kg CO2e / kg'.
-  Example Answer: 'Hakkede tomater: X kg CO2e/ kg \n'.
-- If the ingredient is not found in the database, return '?'.
-  Example Answer: 'Chopped tomatoes: ? \n'
-- Do not provide any ranges for the final answer. For example, do not provide '0.1-0.5 kg CO2e per kg' as the final answer. 
-  Instead, return the closest match.
-
-Use the following format:
-Ingredients: "Ingredients here"
-SQLQuery: "SQL Query to run"
-SQLResult: "Result of the SQLQuery"
-Answer: "Final answer here"
-
-Only use the following tables:
-{table_info}
-
-Begin!
-
-Ingredients:
+DK_INGREDIENTS_EXAMPLE = """
 150 g røde linser
 1 dåse hakkede tomater
 2 terninger grøntsagsbouillon
@@ -104,8 +34,21 @@ Ingredients:
 1. spsk. chilipulver
 10 majstortillas
 1 stjernefrugt
+"""
 
-SQLQuery: SELECT Navn, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE
+EN_SQL_QUERY_EXAMPLE = """
+SELECT Name, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE
+          Name LIKE '%tomato%' OR
+          Name LIKE '%lentil%' OR
+          Name LIKE '%bouillon%' OR
+          Name LIKE '%juice%' OR
+          Name LIKE '%lemon%' OR
+          Name LIKE '%chili%' OR
+          Name LIKE '%starfruit%'
+"""
+
+DK_SQL_QUERY_EXAMPLE = """
+SELECT Navn, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE
           Navn LIKE '%tomat%' OR
           Navn LIKE '%linse%' OR
           Navn LIKE '%bouillon%' OR
@@ -114,8 +57,21 @@ SQLQuery: SELECT Navn, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE
           Navn LIKE '%chili%' OR
           Navn LIKE '%tortilla%' OR
           Navn LIKE '%stjernefrugt%'
+"""
 
-SQLResult: [('Tomat, uspec., rå', 0.7), ('Grønne linser, tørrede', 1.78)
+EN_SQL_RESULT_EXAMPLE = """
+[('Tomato, ripe, raw, origin unknown', 0.7), ('Green lentils, dried', 1.78)
+            ('Tomatojuice, canned', 1.26), ('Tomato, peeled, canned', 1.26)
+            ('Tomato paste, concentrated', 2.48), ('Red lentils, dried', 1.78
+            ('Ice, popsickle, lemonade', 1.15), ('Lemon, raw', 0.94
+            ('Apple juice', 1.64),('Bouillon, chicken, prepared', 0.38)
+            ('Bouillon, beef, prepared', 0.52), ('Pepper, hot chili, raw', 1.02)
+            ('Pepper, hot chili, canned', 1.54),
+            ]
+"""
+
+DK_SQL_RESULT_EXAMPLE = """
+[('Tomat, uspec., rå', 0.7), ('Grønne linser, tørrede', 1.78)
             ('Tomatjuice, konserves', 1.26), ('Tomat, flået, konserves', 1.26)
             ('Tomatpure, koncentreret', 2.48), ('Røde linser, tørrede', 1.78
             ('Ispind, limonade', 1.15), ('Citron, rå', 0.94
@@ -123,8 +79,18 @@ SQLResult: [('Tomat, uspec., rå', 0.7), ('Grønne linser, tørrede', 1.78)
             ('Bouillon, oksekød, spiseklar', 0.52), ('Peber, chili, rå', 1.02)
             ('Tortillabrød, hvede',0.74), ('Peber, chili, konserves', 1.54),
             ]
+"""
 
-Answer:
+EN_FINAL_ANSWER_EXAMPLE = """
+Red lentils: 1.78 kg CO2e / kg
+Chopped tomatoes: 1.26 kg CO2e / kg
+Vegetable bouillon: 0.38 kg CO2e / kg (closest was chicken bouillon)
+Tomato concentrate (140 g): 2.48 kg CO2e / kg (closest was tomato paste)
+Lemon juice: 0.94 kg CO2e / kg (Closest was Lemon, raw)
+Starfruit: ? (Not found in database)
+"""
+
+DK_FINAL_ANSWER_EXAMPLE = """
 Røde linser: 1.78 kg CO2e / kg
 Hakkede tomater: 1.26 kg CO2e / kg
 Grøntsagsbouillon: 0.38 kg CO2e / kg (closest was chicken bouillon)
@@ -132,26 +98,32 @@ Tomatkoncentrat: 2.48 kg CO2e / kg (closest was tomato paste)
 Citronsaft: 0.94 kg CO2e / kg (Closest was Lemon, raw)
 Majstortillas: 0.74 kg CO2e / kg (Closest was Tortillabrød, hvede)
 Stjernefrugt: ? (Not found in database)
+"""
 
-Ingredients: {input}"""
 
-EN_CO2_SQL_PROMPT = """Given a list of ingredients,
+CO2_SQL_PROMPT_TEMPLATE = """Given a list of ingredients,
             extract the main ingredients from the list and create a syntactically correct {dialect} query to run,
             then look at the results of the query and return the answer.
 
 Solve the task using the following steps:
 - Query all ingredients in a single query.
+  Example query: {example_query}
 - In the query, remove all non-ingredient words.
+  Example of removing: {example_removing}
 - Match the SQLResult to the list of ingredients based on preparation and type.
+  Example match: {example_match}
 - Return the Answer in the following format: ''ingredient': X kg CO2e / kg'.
+  Example Answer: {example_answer}
 - If the ingredient is not found in the database, return '?'.
-- Do not return any ranges of possible values, only the closest match.
+  Example Answer: {example_not_found}
+- Do not provide any ranges for the final answer. For example, do not provide '0.1-0.5 kg CO2e per kg' as the final answer.
+  Instead, return the closest match.
 
 Use the following format:
 Ingredients: "Ingredients here"
 SQLQuery: "SQL Query to run"
 SQLResult: "Result of the SQLQuery"
-Answer: "Final answer here"
+Final Answer: "Final answer here"
 
 Only use the following tables:
 {table_info}
@@ -159,41 +131,45 @@ Only use the following tables:
 Begin!
 
 Ingredients:
-150 g red lentils
-1 can of chopped tomatoes
-2 cubes of vegetable bouillon
-1 tin of tomato concentrate (140 g)
-1 tbsp. lemon juice
-1. tbsp. chili powder
-10 corn tortillas
-1 starfruit
+{ingredients_example}
 
-SQLQuery: SELECT Name, Total_kg_CO2_eq_kg FROM dk_co2_emission WHERE
-          Name LIKE '%tomato%' OR
-          Name LIKE '%lentil%' OR
-          Name LIKE '%bouillion%' OR
-          Name LIKE '%juice%' OR
-          Name LIKE '%lemon%' OR
-          Name LIKE '%chili%' OR
-          Name LIKE '%tortilla%' OR
-          Name LIKE '%starfruit%'
+SQLQuery: {query_example}
 
-SQLResult: [('Tomato, ripe, raw, origin unknown', 0.7), ('Green lentils, dried', 1.78)
-            ('Tomatojuice, canned', 1.26), ('Tomato, peeled, canned', 1.26)
-            ('Tomato paste, concentrated', 2.48), ('Red lentils, dried', 1.78
-            ('Ice, popsickle, lemonade', 1.15), ('Lemon, raw', 0.94
-            ('Apple juice', 1.64),('Bouillon, chicken, prepared', 0.38)
-            ('Bouillon, beef, prepared', 0.52), ('Pepper, hot chili, raw', 1.02)
-            ('Tortilla bread, wheat',0.74),('Pepper, hot chili, canned', 1.54),
-            ]
+SQLResult: {query_result_example}
 
-Answer:
-Red lentils: 1.78 kg CO2e / kg
-Chopped tomatoes: 1.26 kg CO2e / kg
-Vegetable bouillon: 0.38 kg CO2e / kg (closest was chicken bouillon)
-Tomato concentrate (140 g): 2.48 kg CO2e / kg (closest was tomato paste)
-Lemon juice: 0.94 kg CO2e / kg (Closest was Lemon, raw)
-Corn tortillas: 0.74 kg CO2e / kg (Closest was Tortilla bread, wheat)
-Starfruit: ? (Not found in database)
+Final Answer:
+{final_answer_example}
 
 Ingredients: {input}"""
+
+EN_CO2_SQL_PROMPT_TEMPLATE = PromptTemplate(
+    template=CO2_SQL_PROMPT_TEMPLATE,
+    input_variables=["dialect", "table_info", "input"],
+    partial_variables={
+        "example_query": EN_SQL_QUERY_EXAMPLE,
+        "example_removing": EN_EXAMPLE_REMOVING,
+        "example_match": EN_EXAMPLE_MATCH,
+        "example_answer": EN_EXAMPLE_ANSWER_FOUND,
+        "example_not_found": EN_EXAMPLE_ANSWER_NOT_FOUND,
+        "ingredients_example": EN_INGREDIENTS_EXAMPLE,
+        "query_example": EN_SQL_QUERY_EXAMPLE,
+        "query_result_example": EN_SQL_RESULT_EXAMPLE,
+        "final_answer_example": EN_FINAL_ANSWER_EXAMPLE,
+    },
+)
+
+DK_CO2_SQL_PROMPT_TEMPLATE = PromptTemplate(
+    template=CO2_SQL_PROMPT_TEMPLATE,
+    input_variables=["dialect", "table_info", "input"],
+    partial_variables={
+        "example_query": DK_SQL_QUERY_EXAMPLE,
+        "example_removing": DK_EXAMPLE_REMOVING,
+        "example_match": DK_EXAMPLE_MATCH,
+        "example_answer": DK_EXAMPLE_ANSWER_FOUND,
+        "example_not_found": DK_EXAMPLE_ANSWER_NOT_FOUND,
+        "ingredients_example": DK_INGREDIENTS_EXAMPLE,
+        "query_example": DK_SQL_QUERY_EXAMPLE,
+        "query_result_example": DK_SQL_RESULT_EXAMPLE,
+        "final_answer_example": DK_FINAL_ANSWER_EXAMPLE,
+    },
+)
